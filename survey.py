0615a2072924d82969bc0970f9cc3e4ba894624d
z-shu <75577537+z-shu@users.noreply.github.com>
2022-06-20 23:16:51 +0100

    remove file

--- survey.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-survey_df = pd.read_csv(r"C:\Users\zukhr\Downloads\survey.csv")
-
-# head
-print(survey_df.tail())
-print(survey_df.info())
-print(survey_df.head())
-#print(survey_df.columns)
